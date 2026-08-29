6f135a192b58ecc58f759b5fa81a9a259da7d604
JefersonBatista <jeferson.de.oliveira.batista@gmail.com>
2026-08-29 11:44:59 -0300

    feat: request for num chapters of each book

--- pages/api/num_chapters.js
+++ pages/api/num_chapters.js
@@ -8,23 +8,10 @@ export default async function handler(req, res) {
 
   const books = await response.json();
 
-  // Adding up chapters by testament
-  let ot_num_chapters = 0;
-  let nt_num_chapters = 0;
+  const report = books.map((book) => ({
+    name: book.name,
+    chapters: book.chapters,
+  }));
 
-  books.map((book) => {
-    if (book.testament === "VT") {
-      ot_num_chapters += book.chapters;
-    } else {
-      nt_num_chapters += book.chapters;
-    }
-  });
-
-  const number_of_chapters = ot_num_chapters + nt_num_chapters;
-
-  res.status(200).json({
-    "Capítulos do Antigo Testamento": ot_num_chapters,
-    "Capítulos do Novo Testamento": nt_num_chapters,
-    "Total de capítulos da Bíblia": number_of_chapters,
-  });
+  res.status(200).json(report);
 }
